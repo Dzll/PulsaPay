e0e8ffba8d6f53ad8331aed93336960d3e007ee7
Dzul <dzulkarnainc12@gmail.com>
2019-01-19 18:40:24 +0700

    Iseng Iseng

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -66,44 +66,35 @@ class DatabaseSeeder extends Seeder
         // $transaksi->harga_pulsa = '5400';
         // $transaksi->save();
 
-        // $user = new User();
-        // $user->name = 'dzll';
-        // $user->email = 'dzll@gmail.com';
-        // $user->no_hp = '08886657756';
-        // $user->password = Hash::make('123456');
-        // $user->remember_token = Hash::make($user->email);
-        // $user->save();
+        $pulsa = new PulsaData();
+        $pulsa->jumlah_pulsa = '5000';
+        $pulsa->masa_aktif = 'masa aktif 7 hari';
+        $pulsa->harga_pulsa = '5500';
+        $pulsa->save();
 
-        // $user = new User();
-        // $user->name = 'aji subarkah';
-        // $user->email = 'ajisubarkah99@gmail.com';
-        // $user->no_hp = '085940900559';
-        // $user->password = Hash::make('123456');
-        // $user->save();
+        $pulsa = new PulsaData();
+        $pulsa->jumlah_pulsa = '10000';
+        $pulsa->masa_aktif = 'masa aktif 15 hari';
+        $pulsa->harga_pulsa = '11000';
+        $pulsa->save();
 
-        // $pulsa = new PulsaData();
-        // $pulsa->jumlah_pulsa = '5000';
-        // $pulsa->masa_aktif = 'masa aktif 7 hari';
-        // $pulsa->harga_pulsa = '5500';
-        // $pulsa->save();
+        $pulsa = new PulsaData();
+        $pulsa->jumlah_pulsa = '20000';
+        $pulsa->masa_aktif = 'masa aktif 20 hari';
+        $pulsa->harga_pulsa = '20500';
+        $pulsa->save();
 
-        // $pulsa = new PulsaData();
-        // $pulsa->jumlah_pulsa = '10000';
-        // $pulsa->masa_aktif = 'masa aktif 15 hari';
-        // $pulsa->harga_pulsa = '11000';
-        // $pulsa->save();
-
-        // $pulsa = new PulsaData();
-        // $pulsa->jumlah_pulsa = '25000';
-        // $pulsa->masa_aktif = 'masa aktif 30 hari';
-        // $pulsa->harga_pulsa = '24900';
-        // $pulsa->save();
-
-        // $pulsa = new PulsaData();
-        // $pulsa->jumlah_pulsa = '50000';
-        // $pulsa->masa_aktif = 'masa aktif 30 hari';
-        // $pulsa->harga_pulsa = '49000';
-        // $pulsa->save();
+        $pulsa = new PulsaData();
+        $pulsa->jumlah_pulsa = '25000';
+        $pulsa->masa_aktif = 'masa aktif 30 hari';
+        $pulsa->harga_pulsa = '24900';
+        $pulsa->save();
+		
+		$pulsa = new PulsaData();
+        $pulsa->jumlah_pulsa = '30000';
+        $pulsa->masa_aktif = 'masa aktif 30 hari';
+        $pulsa->harga_pulsa = '31000';
+        $pulsa->save();
 
         // $pulsapay = new PulsaPay();
         // $pulsapay->id_pulsapay = '1';
